11bdd47fe7d0347379aaf1f267c9b4dadc1b6f85
Nerian <richard080405@gmail.com>
2025-11-15 16:34:37 -0500

    Se anadió el cálculo de las deducciones.

--- src/main/java/com/udc/act/actividad3/Empleado.java
+++ src/main/java/com/udc/act/actividad3/Empleado.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.udc.act.actividad3;
 
-/**
- *
- * @author LENOVO84
- */
 public abstract class Empleado { //Debe ser abstracta para que no se pueda instanciar, ya que no se pueden crear empleados genéricos sin tipo.
     protected String nombre;
     protected double salarioBruto; //Salario antes de impuestos
@@ -15,6 +7,16 @@ public abstract class Empleado { //Debe ser abstracta para que no se pueda insta
     public Empleado(String nombre) {
         this.nombre = nombre;
     }
+
+    public abstract double calcularSalarioBruto();
+    public abstract double calcularBeneficios();
+
+    public double calcularDeducciones(double bruto){
+        double SSYPension = bruto * 0.04; //Seguro Social y Pensión 
+        double arl = bruto * 0.00522; //En este caso, usaremos el ARL de riesgo 1
+        return SSYPension + arl;
+    }
+
 
     
 
